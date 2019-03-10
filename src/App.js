4f4a0b833f05cf6dc754197c283a118f228bbe5e
kowalsska <kowalsska2@gmail.com>
2019-03-11 00:19:36 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+import Events from './components/Events'
+import SearchBar from './components/SearchBar'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+})
 
-export default App;
+const App = () => (
+  <ApolloProvider client={client}>
+    <SearchBar />
+    <div className="container">
+      <Events />
+    </div>
+  </ApolloProvider>
+)
+
+export default App
